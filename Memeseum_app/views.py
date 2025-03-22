f3fb94feda2e3c434af835393661624de8041c91
Jp-moraiss <164018039+Jp-moraiss@users.noreply.github.com>
2025-03-22 20:18:56 -0300

    Freeze Requirements + def Add

--- Memeseum_app/views.py
+++ Memeseum_app/views.py
@@ -1,9 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Meme
+from django.contrib import messages
 
 def home(request):
     return render(request,'index.html')
 
 def add(request):
+    if request.method == 'POST':
+        nome = request.POST.get('nome')
+        descricao = request.POST.get('descricao')
+        ano = request.POST.get('ano')
+        imagem = request.POST.get('imagem')
+
+        Meme = Meme(
+            nome = nome,
+            descricao = descricao,
+            ano = ano,
+            imagem = imagem
+        )
+
+        Meme.save()
+        messages.success(request, "Meme adicionado com sucesso!")
+        return redirect ('home')
+
     return render(request,'add.html')
 
 def ver(request):
